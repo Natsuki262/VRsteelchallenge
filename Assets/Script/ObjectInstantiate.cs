8225dc9f0aaaaf286aeb862b82cd6d0959697bee
fgl0001 <hiro.58.fgl.2525.952@icloud.com>
2022-02-22 22:19:03 +0900

    銃から銃弾を生成することができた次は発射

---
+++ Assets/Script/ObjectInstantiate.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInstantiate : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    /// <summary>オブジェクト生成</summary>
+    public void InstantiateObject()
+    {
+        Instantiate(m_prefab, transform.position, transform.rotation);
+    }
+}
