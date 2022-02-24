f9894ea829611dfecbb73b1c4f955873b3ba3245
fgl0001 <hiro.58.fgl.2525.952@icloud.com>
2022-02-25 02:38:51 +0900

    ひとまず第一部完成A版

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -8,9 +8,7 @@ public class Bullet : MonoBehaviour
     float m_speed;
     Rigidbody m_rb;
     [SerializeField]
-    GameObject m_prefab;
-    [SerializeField]
-    float m_deleateTime=5;
+    float m_deleateTime = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,13 +19,20 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     /// <summary>èeíeÇâ¡ë¨Ç≥ÇπÇÈ</summary>
-   public void AddForceBullet()
+    public void AddForceBullet()
     {
-        m_rb=GetComponent<Rigidbody>();
+        m_rb = GetComponent<Rigidbody>();
         Vector3 vector = transform.forward * m_speed;
-        m_rb.AddForce(vector,ForceMode.Impulse);
+        m_rb.AddForce(vector, ForceMode.Impulse);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Target")
+        {
+            Destroy(gameObject);
+        }
     }
 }
