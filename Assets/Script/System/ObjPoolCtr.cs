0b2624b21a9705b806b68fee386a1c8df02a5363
fgl0001 <hiro.58.fgl.2525.952@icloud.com>
2022-06-27 14:09:25 +0900

    いらないものを削除してオブジェクトプールさくせい中

---
+++ Assets/Script/System/ObjPoolCtr.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjPoolCtr : MonoBehaviour
+{
+    /// <summary>
+    /// ’™‚ß‚Ä‚¨‚­‚à‚Ì
+    /// </summary>
+    [SerializeField]
+    GameObject poolObj;
+    /// <summary>
+    /// ’™‚ß‚Ä’u‚­ŒÂ”
+    /// </summary>
+    [SerializeField]
+    int poolNum;
+    /// <summary>
+    /// ŠÇ—‚µ‚â‚·‚¢‚æ‚¤‚ÉƒŠƒXƒg‰»
+    /// </summary>
+    public List<GameObject> poolList = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < poolNum; i++)
+        {
+            GameObject obj = Instantiate(poolObj,this.gameObject.transform);
+            obj.SetActive(false);
+            poolList.Add(obj);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
