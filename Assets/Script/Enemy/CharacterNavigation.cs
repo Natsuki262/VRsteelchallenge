f4e40d172880885813c23cedaede20b3fe30e0f0
fgl0001 <hiro.58.fgl.2525.952@icloud.com>
2022-11-07 18:11:25 +0900

    ゾンビ巡回システム完了

--- Assets/Script/Enemy/CharacterNavigation.cs
+++ Assets/Script/Enemy/CharacterNavigation.cs
@@ -6,17 +6,36 @@ using UnityEngine.AI;
 public class CharacterNavigation : MonoBehaviour
 {
     [SerializeField]
-    private Transform m_target;
-    private NavMeshAgent m_Agent;
+    Transform[] m_points;
+    private int destPoint = 0;
+    private NavMeshAgent m_agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        m_Agent = GetComponent<NavMeshAgent>();
+        m_agent = GetComponent<NavMeshAgent>();
+        m_agent.autoBraking = false;
+        GoNextPoint();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_Agent.SetDestination(m_target.position);
+       if(!m_agent.pathPending&&m_agent.remainingDistance<0.5f)
+        {
+            GoNextPoint();
+        }
+    }
+    void GoNextPoint()
+    {
+        if (m_points.Length == 0)
+            return;
+        //エージェントが現在地点に設定された目標地点に行くようにする
+        m_agent.destination = m_points[destPoint].position;
+
+        //配列内の次の位置を目標地点を設定し、
+        //次の目標ならば出発地点に戻ります。
+        destPoint=(destPoint+1)%m_points.Length;
     }
 }
