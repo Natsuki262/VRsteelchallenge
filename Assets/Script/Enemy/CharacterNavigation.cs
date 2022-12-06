873bab89e271a620a489d7721c71dfb2e8add5c1
fgl0001 <hiro.58.fgl.2525.952@icloud.com>
2022-12-06 18:30:53 +0900

    ナビメッシュを使って巡回、プレイヤーを見つけたら追跡、範囲を離れたらまた巡回ポイントに戻るスクリプトを書いているが追跡はするが、巡回に戻らない

--- Assets/Script/Enemy/CharacterNavigation.cs
+++ Assets/Script/Enemy/CharacterNavigation.cs
@@ -3,39 +3,84 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
-public class CharacterNavigation :EnemyBase
+public class CharacterNavigation : EnemyBase
 {
+    /// <summary>
+    /// 巡回する目標ポイントの配列
+    /// </summary>
     [SerializeField]
     Transform[] m_points;
-    private int destPoint = 0;
+    /// <summary>
+    /// 現在の目標地点のインデックス
+    /// </summary>
+    private int m_destPoint = 0;
+   /// <summary>
+   /// ナビメッシュエージェント
+   /// </summary>
     private NavMeshAgent m_agent;
+    /// <summary>
+    /// プレイヤーを表す変数
+    /// </summary>
+    [SerializeField]
+    private Transform m_player;
+    /// <summary>
+    /// プレイヤーを検知する距離
+    /// </summary>
+    public float m_followRange = 5f;
+    /// <summary>
+    /// プレイヤーへの追跡中かどうかを表すフラグ
+    /// </summary>
+    private bool m_isChasing = false;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         m_agent = GetComponent<NavMeshAgent>();
         m_agent.autoBraking = false;
-        GoNextPoint();
-        
+        //巡回する座標ポイントが設定されている場合は、
+        //次の目標地点へ移動する
+        if (m_points.Length > 0) GoNextPoint();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(!m_agent.pathPending&&m_agent.remainingDistance<0.5f)
+        float ditance=Vector3.Distance(transform.position, m_player.position);
+        if (ditance<=m_followRange)
         {
-            GoNextPoint();
+
+            //プレイヤーへの追跡中フラグを設定する
+            m_isChasing = true;
+
+            // 現在の目標地点を次の目標地点に更新する
+            m_agent.destination = m_player.position;
+
+
         }
+        else if (m_isChasing)
+        {
+            //プレイヤー追跡中フラグを解除
+            m_isChasing = false;
+            //次の目標地点へ移動する
+            GoNextPoint();
+            
+            
+        }
+
+        
     }
     void GoNextPoint()
     {
-        if (m_points.Length == 0)
-            return;
-        //エージェントが現在地点に設定された目標地点に行くようにする
-        m_agent.destination = m_points[destPoint].position;
+        //座標ポイントが設定されていない場合なにもしない
+        if (m_points.Length == 0)return;
 
-        //配列内の次の位置を目標地点を設定し、
-        //次の目標ならば出発地点に戻ります。
-        destPoint=(destPoint+1)%m_points.Length;
+        //現在の目標地点の移動地点からの次の目標地点を計算する
+        m_destPoint = (m_destPoint + 1) % m_points.Length;
+
+        //現在の目標地点を更新する
+        m_agent.destination = m_points[m_destPoint].position;
     }
 }
