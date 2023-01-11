49bb51702d86b608a215a37e15b6e493c4e9ce95
fgl0001 <hiro.58.fgl.2525.952@icloud.com>
2023-01-11 11:40:37 +0900

    ゾンビの巡回AI作成しなおし

--- Assets/Script/Enemy/ZonbieController.cs
+++ Assets/Script/Enemy/ZonbieController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class ZonbieContr : MonoBehaviour
+public class ZonbieController : MonoBehaviour
 {
     //アニメーターとAI
     [SerializeField]
@@ -26,14 +26,55 @@ public class ZonbieContr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        SwichState();
     }
+
     //Animationストップする関数
     public void AnimationStop()
-        {
+    {
         m_animator.SetBool("waking", false);
         m_animator.SetBool("Atack", false);
         m_animator.SetBool("Run", false);
         m_animator.SetBool("Deth", false);
+    }
+    void SwichState()
+    {
+        switch (state)
+        {
+            case STATE.IDLE:
+                AnimationStop();
+                if (Random.Range(0, 5000) < 5)
+                {
+                    state = STATE.PATROL;
+                }
+                break;
+            case STATE.PATROL:
+                if (!m_agent.hasPath)
+                {
+                    float x = transform.position.x + Random.Range(-5, 5);
+                    float z = transform.position.z + Random.Range(-5, 5);
+
+                    Vector3 NextPos=new Vector3(x,transform.position.y,z);
+
+                    m_agent.SetDestination(NextPos);
+                    m_agent.stoppingDistance = 0;
+                    AnimationStop();
+
+                    m_agent.speed = m_enemySpeed;
+                    m_animator.SetBool("waking", true);
+                }
+                if(Random.Range(0,5000)<5)
+                {
+                    state=STATE.IDLE;
+                    m_agent.ResetPath();
+                }
+                break;
+            case STATE.ATTACK:
+                break;
+            case STATE.DED:
+                break;
+            default:
+                break;
+        }
     }
 }
